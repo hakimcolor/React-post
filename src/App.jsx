bfd7d12d811eb123464d691be6d62f1ea37ec61c
hakimcolor <hakimcolor777@gmail.com>
2025-09-19 17:26:08 +0600

    my commit for change somthing

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Counter from './Counter';
 
 export default function App() {
-  const handelclik = (num) => {
-    const number = num + 5;
-    alert(
-      `Your number was 5 and your button number was 50. When you clicked the button, your number ${num} + 5 = ${number}`
-    );
-  };
+  // const handelclik = (num) => {
+  //   const number = num + 5;
+  //   alert(
+  //     `Your number was 5 and your button number was 50. When you clicked the button, your number ${num} + 5 = ${number}`
+  //   );
+  // };
 
 
   return (
@@ -15,12 +15,12 @@ export default function App() {
       <div className="text-center">
         <h1>useStste hook</h1>
         <Counter />
-        <button
+        {/* <button
           onClick={() => handelclik(50)}
           className="p-4 border-2 border-red-600 rounded-xl mt-5 cursor-pointer hover:bg-cyan-500 hover:border-none w-20 h-20 transition duration-1000"
         >
           click
-        </button>
+        </button> */}
       </div>
 
     
